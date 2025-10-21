1bae6b663f6380e29d5160eb62b2fff059999911
kevin cuadros <kevindavidcuadros@gmail.com>
2025-10-20 19:02:47 -0500

    inicio reto 6

--- src/adventJS/main.ts
+++ src/adventJS/main.ts
@@ -3,6 +3,7 @@ import { createFrame } from "./02-enmarcando-nombres";
 import { organizeInventory } from "./03-organizando-inventario";
 import { createXmasTree } from "./04-decorando-arbol-navidad";
 import { organizeShoes } from "./05-organizando-zapatos";
+import { inBox } from "./06-dentro-de-caja";
 
 // console.log(prepareGifts([3, 1, 2, 3, 4, 2, 5]))
 
@@ -19,22 +20,36 @@ import { organizeShoes } from "./05-organizando-zapatos";
 
 // console.log(createXmasTree(5, '+'))
 
-const shoes: any = [
-  { type: 'I', size: 38 },
-  { type: 'R', size: 38 },
-  { type: 'I', size: 38 },
-  { type: 'I', size: 38 },
-  { type: 'R', size: 38 }
-]
+// const shoes: any = [
+//   { type: 'I', size: 38 },
+//   { type: 'R', size: 38 },
+//   { type: 'I', size: 38 },
+//   { type: 'I', size: 38 },
+//   { type: 'R', size: 38 }
+// ]
 // [38, 38]
 // console.log(organizeShoes(shoes))
 
-const shoes2: any = [
-  { type: 'I', size: 38 },
-  { type: 'R', size: 38 },
-  { type: 'R', size: 42 },
-  { type: 'I', size: 41 },
-  { type: 'I', size: 42 }
-]
+// const shoes2: any = [
+//   { type: 'I', size: 38 },
+//   { type: 'R', size: 38 },
+//   { type: 'R', size: 42 },
+//   { type: 'I', size: 41 },
+//   { type: 'I', size: 42 }
+// ]
 // [38, 42]
-console.log(organizeShoes(shoes2))
+// console.log(organizeShoes(shoes2))
+
+
+console.log(inBox([
+  "###",
+  "#*#",
+  "###"
+]))
+
+console.log(inBox([
+  "#####",
+  "#   #",
+  "#  #*",
+  "#####"
+]))
